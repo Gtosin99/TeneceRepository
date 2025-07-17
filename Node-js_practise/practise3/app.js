2f880c46c9f6a2f05c406f5a8bdec082ab2f3fb5
t.akindeleCOS101 <tosinakindele826@gmail.com>
2025-07-17 23:16:42 +0100

    EJS commit

--- Node-js_practise/practise3/app.js
+++ Node-js_practise/practise3/app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const parser=require('body-parser')
 const path=require('path')
 
+
 const admindata=require('./routes/admin')
 const shoproutes=require('./routes/shop')
 const rootdir=require('./util/path')
 
 const app = express();
 
-app.set('view engine','pug')  // app.set() allows to set and get global values of app 
-                              //but that isnt the use here we are using it to define the templating engine
+app.set('view engine','ejs')  
 app.set('views','views')   // if you didnt name your html folder views you have to declare this here with what you named it
 app.use(parser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'public')))  //for linking stylesheet
@@ -19,7 +19,7 @@ app.use(admindata.routes)
 app.use(shoproutes)
 
 app.use((req,res,next)=>{
-    res.status(404).render('404')
+    res.status(404).render('404',{pageTitle:'Page Not Found'})
 })
 
 app.listen(3000)
--- Node-js_practise/practise3/routes/shop.js
+++ Node-js_practise/practise3/routes/shop.js
@@ -6,7 +6,7 @@ const admindata=require('./admin')
 
 router.get('/',(req,res,next)=>{
     const products = admindata.products
-    res.render('shop.pug',{prods:products,docTitle:'Shop'})  //function from express that uses default templating engine listed in app.js
+    res.render('shop',{prods:products,pageTitle:'Shop'})  //function from express that uses default templating engine listed in app.js
                     //also used to pass content into the template
 })
 
